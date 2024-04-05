9007b237b3411238125008c002efe159332e79ab
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-05 23:04:20 +0200

    13 session part 2

---
+++ 13_session_part_2/src/data/index.js
@@ -0,0 +1,11 @@
+export const users = [
+  { id: 1, name: "Johna Doe", username: "johnD", password: "secret" },
+  { id: 2, name: "Anna Smith", username: "an88na", password: "secret" },
+  { id: 3, name: "Mike Tyson", username: "micky_mouse", password: "secret" },
+];
+
+export const products = [
+  { id: 1, name: "cheese" },
+  { id: 2, name: "eggs" },
+  { id: 3, name: "milk" },
+];
